eb9f1e082d83e939157f8f5b257916bd5448d41b
Rémi Assailly <remi@assailly.com>
2026-08-19 09:52:56 +0200

    ajout email dans l'en-tête copyright

--- src/protocol/framing.c
+++ src/protocol/framing.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2026 Rémi
+ * Copyright (c) 2026 Rémi Assailly
+ * Rémi@assailly.com
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
