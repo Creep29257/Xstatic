1cc97108086a7615b8bc600ddfd7cbef9879f5ed
Remi ASSAILLY <remi@assailly.com>
2026-09-23 10:40:02 +0200

    fix empty channel_hash.c (forgot to save)

---
+++ src/core/channel_hash.c
@@ -0,0 +1,64 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2026, Rémi
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdbool.h>
+#include <string.h>
+#include "core/channel_hash.h"
+#include "pb.h"
+
+
+uint8_t channel_hash_compute(const channel_slot_t *slot)
+{
+uint8_t hash = 0;
+
+for(size_t i = 0; i<sizeof(slot->name); i ++)
+    {
+        hash ^= slot->name[i];
+    }
+for (pb_size_t i=0; i<slot->psk.size; i++)
+    {
+        hash ^= slot->psk.bytes[i];
+    }
+    return hash;
+    
+}
+
+int channel_hash_find_index(const channel_state_t *cstate, uint8_t hash)
+{
+    int index = -1;
+    for(int i=0; i<8;i++)
+    {
+        if(cstate->channels[i].hash == hash)
+        {
+            index = i;
+            return index;
+        }
+    }
+    return index;
+}
